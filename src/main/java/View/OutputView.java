7ddc3a57f3dec5113bd97b0f06deea82ada087a0
minoooo119 <minoth0119@naver.com>
2023-11-13 23:31:43 +0900

    [FEAT] OutputView.java: 모든 title 출력할 함수 추가

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,11 +1,47 @@
 package View;
 
+import christmas.MenuDetail;
+
+import java.util.List;
+
+import static View.OutputComment.*;
+
+
 public class OutputView {
     private final Integer date;
-    public OutputView(Integer date){
+    public final List<MenuDetail> menuDetailList;
+
+    public OutputView(Integer date, List<MenuDetail> menuDetailList){
         this.date=date;
+        this.menuDetailList=menuDetailList;
     }
     public void printOpeningTitle(){
-        System.out.println("12월 "+this.date+"일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.printf((OPENING_TITLE) + "%n", date);
+    }
+    public void printMenuTitle(){
+        System.out.println(MENU_TITLE);
+    }
+    public void printOriginalPriceTitle(){
+        System.out.println(ORIGINAL_PRICE_TITLE);
+    }
+    public void printGiftMenuTitle(){
+        System.out.println(GIFT_MENU_TITLE);
+    }
+    public void printBenefitDetailsTitle(){
+        System.out.println(BENEFIT_DETAILS_TITLE);
+    }
+    public void printTotalBenefitPriceTitle(){
+        System.out.println(TOTAL_BENEFIT_PRICE_TITLE);
+    }
+    public void printFinalPriceTitle(){
+        System.out.println(FINAL_PRICE_TITLE);
+    }
+    public void printDecemberEventBadgeTitle(){
+        System.out.println(DECEMBER_EVENT_BADGE_TITLE);
+    }
+    public void printMenuDetail(){
+        for(MenuDetail menuDetail:menuDetailList){
+            System.out.println(menuDetail.name+" "+menuDetail.num+"개");
+        }
     }
 }
