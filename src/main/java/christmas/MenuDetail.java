b4e1099b8b7d32afe73e5294aaf08b4922593d6c
minoooo119 <minoth0119@naver.com>
2023-11-13 20:18:30 +0900

    [ADD] Menu 내용에 대한 enum class 추가 MenuGroup(그룹 별 메뉴), MenuDetail(메뉴 & 가격))

---
+++ src/main/java/christmas/MenuDetail.java
@@ -0,0 +1,23 @@
+package christmas;
+
+public enum MenuDetail {
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000),
+    MUSHROOM_SOUP("양송이수프", 6000),
+    T_BONE_STEAK("티본스테이크", 55000),
+    BABY_BACK_RIBS("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CHOCOLATE_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+    ZERO_COLA("제로콜라", 3000),
+    RED_WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000),
+    EMPTY("0", 0);
+    public final String name;
+    public final int price;
+    MenuDetail(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/main/java/christmas/MenuGroup.java
@@ -0,0 +1,30 @@
+package christmas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum MenuGroup {
+    MAIN("메인", Arrays.asList(MenuDetail.BABY_BACK_RIBS, MenuDetail.CHRISTMAS_PASTA, MenuDetail.SEAFOOD_PASTA, MenuDetail.T_BONE_STEAK)),
+    APPETIZER("애피타이저", Arrays.asList(MenuDetail.CAESAR_SALAD, MenuDetail.MUSHROOM_SOUP, MenuDetail.TAPAS)),
+    DESSERT("디저트", Arrays.asList(MenuDetail.CHOCOLATE_CAKE, MenuDetail.ICE_CREAM)),
+    DRINK("음료", Arrays.asList(MenuDetail.CHAMPAGNE, MenuDetail.RED_WINE, MenuDetail.ZERO_COLA)),
+    EMPTY("없음", Arrays.asList(MenuDetail.EMPTY));
+
+    private final String category;
+    private final List<MenuDetail> menuList;
+
+    MenuGroup(String category, List<MenuDetail> menuList) {
+        this.category = category;
+        this.menuList = menuList;
+    }
+    public static MenuGroup findByMenuList(String pickMenu){
+        return Arrays.stream(MenuGroup.values())
+                .filter(menu -> menu.menuList.contains(pickMenu))
+                .findAny()
+                .orElse(EMPTY);
+    }
+    public boolean hasPickMenu(String pickMenu){
+        return menuList.stream()
+                .anyMatch(menu -> menu.equals(pickMenu));
+    }
+}
