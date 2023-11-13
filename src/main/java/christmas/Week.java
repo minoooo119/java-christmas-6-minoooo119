0ca91ba5affa0ef23ad263f02428c6572bbf9f2b
minoooo119 <minoth0119@naver.com>
2023-11-14 00:30:40 +0900

    [ADD] Day enum class, Week enum class 추가, 12월 날짜 제어 용이

---
+++ src/main/java/christmas/Week.java
@@ -0,0 +1,29 @@
+package christmas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Week {
+    WEEKEND("주말", List.of(Day.SATURDAY, Day.SUNDAY)),
+    WEEKDAY("평일", List.of(Day.MONDAY, Day.TUESDAY, Day.WEDNESDAY, Day.THURSDAY, Day.FRIDAY));
+
+    private final String day;
+    private final List<Day> days;
+
+    Week(String day, List<Day> days) {
+        this.day = day;
+        this.days = days;
+    }
+
+    public static Week findByDay(Day day){
+        return Arrays.stream(Week.values())
+                .filter(week -> week.hasDay(day))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 날짜입니다."));
+    }
+    private boolean hasDay(Day day){
+        return days.stream()
+                .anyMatch(week -> week==day);
+    }
+
+}
