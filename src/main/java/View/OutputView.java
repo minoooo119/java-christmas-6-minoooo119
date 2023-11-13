3cfe52d2263640435747c8fed23b4040df63fa44
minoooo119 <minoth0119@naver.com>
2023-11-13 23:09:20 +0900

    [FEAT] OutputView.java: openingtilte 출력하는 기능 추가

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,5 +1,11 @@
 package View;
 
 public class OutputView {
-
+    private final Integer date;
+    public OutputView(Integer date){
+        this.date=date;
+    }
+    public void printOpeningTitle(){
+        System.out.println("12월 "+this.date+"일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
 }
