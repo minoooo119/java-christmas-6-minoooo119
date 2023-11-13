b4addaffb939aa577025c188bf6ad5447ba2276c
minoooo119 <minoth0119@naver.com>
2023-11-13 23:30:42 +0900

    [ADD] OutputComment.java: 각 title에 맞는 출력 결과 추가

--- src/main/java/View/OutputComment.java
+++ src/main/java/View/OutputComment.java
@@ -1,2 +1,12 @@
-package View;public class OutputComment {
+package View;
+
+public class OutputComment {
+    public static final String OPENING_TITLE="12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String MENU_TITLE="<주문 메뉴>";
+    public static final String ORIGINAL_PRICE_TITLE="<할인 전 총주문 금액>";
+    public static final String GIFT_MENU_TITLE="<증정 메뉴>";
+    public static final String BENEFIT_DETAILS_TITLE="<혜택 내역>";
+    public static final String TOTAL_BENEFIT_PRICE_TITLE="<총혜택 금액>";
+    public static final String FINAL_PRICE_TITLE="<할인 후 예상 결제 금액>";
+    public static final String DECEMBER_EVENT_BADGE_TITLE="<12월 이벤트 배지>";
 }
