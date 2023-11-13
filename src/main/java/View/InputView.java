e9af54e594d2c88aec6318865ec7f1341a2e29ba
minoooo119 <minoth0119@naver.com>
2023-11-13 19:32:37 +0900

    [FIX] InputView.java: date 입력시 error 상황 내용 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -6,7 +6,26 @@ public class InputView {
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        return Integer.parseInt(input);
+        try {
+            return validateDate(validateConvertToInt(input));
+        } catch (IllegalArgumentException e) {
+            return readDate();
+        }
+    }
+    private Integer validateConvertToInt(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자만 기입해주세요");
+            throw new IllegalArgumentException();
+        }
+    }
+    private Integer validateDate(Integer date) {
+        if (date < 1 || date > 31) {
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException();
+        }
+        return date;
     }
     public String readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
