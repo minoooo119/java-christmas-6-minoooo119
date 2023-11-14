a8c2966b97f95a0469d96b15008c45e6c88c953e
minoooo119 <minoth0119@naver.com>
2023-11-14 10:27:08 +0900

    [FIX] CalculateDiscount.java: 만원 이상일 경우 discount 계산하도록 수정

--- src/main/java/Model/CalculateDiscount.java
+++ src/main/java/Model/CalculateDiscount.java
@@ -8,11 +8,17 @@ import christmas.Week;
 import java.util.List;
 
 public class CalculateDiscount {
+    //기본적으로 totalPrice가 만원 이상이어야 할인 계산을 해
     private final List<MenuDetail> menuDetailList;
     private final Integer date;
     private final Integer totalPrice;
     private final Day day;
     private final Week week;
+    private int d_dayDiscount=0;
+    private int weekdayDiscount=0;
+    private int weekendDiscount=0;
+    private int specialStarDiscount=0;
+    private int giftDiscount=0;
     public CalculateDiscount(Integer date, List<MenuDetail> menuDetailList, Integer totalPrice){
         this.date=date;
         this.menuDetailList=menuDetailList;
@@ -20,37 +26,64 @@ public class CalculateDiscount {
         day=Day.findByDate(date);
         week=Week.findByDay(day);
     }
-    public int calculateD_dayDiscount(){
+    public boolean calculateAllDiscount(){
+        if(totalPrice>=10_000){
+            d_dayDiscount=calculateD_dayDiscount();
+            weekdayDiscount=calculateWeekdayDiscount();
+            weekendDiscount=calculateWeekendDiscount();
+            specialStarDiscount=calculateSpecialStarDiscount();
+            giftDiscount=calculateGiftDiscount();
+            return (d_dayDiscount!=0||weekdayDiscount!=0||weekendDiscount!=0||specialStarDiscount!=0||giftDiscount!=0);
+        }
+        return false;
+    }
+    public int getD_dayDiscount(){
+        return d_dayDiscount;
+    }
+    public int getWeekdayDiscount(){
+        return weekdayDiscount;
+    }
+    public int getWeekendDiscount(){
+        return weekendDiscount;
+    }
+    public int getSpecialStarDiscount(){
+        return specialStarDiscount;
+    }
+    public int getGiftDiscount(){
+        return giftDiscount;
+    }
+
+    private int calculateD_dayDiscount(){
         if(date>=1&&date<=25){
             return 1000+100*(date-1);
         }
         return 0;
     }
-    public int calculateWeekdayDiscount(){
+    private int calculateWeekdayDiscount(){
         if(week==Week.WEEKDAY){
             return calculateDessertNum()*2023;
         }
         return 0;
     }
-    public int calculateWeekendDiscount(){
+    private int calculateWeekendDiscount(){
         if(week==Week.WEEKEND){
             return calculateMainNum()*2023;
         }
         return 0;
     }
-    public int calculateSpecialStarDiscount(){
+    private int calculateSpecialStarDiscount(){
         if(date==25||day==Day.SUNDAY){
             return 1000;
         }
         return 0;
     }
-    public int calculateGiftDiscount(){
+    private int calculateGiftDiscount(){
         if(totalPrice>=120000){
             return 25_000;
         }
         return 0;
     }
-    public int calculateDessertNum(){
+    private int calculateDessertNum(){
         int dessertNum=0;
         for(MenuDetail menuDetail:menuDetailList){
             if(MenuGroup.findByMenuList(menuDetail)== MenuGroup.DESSERT){
@@ -59,7 +92,7 @@ public class CalculateDiscount {
         }
         return dessertNum;
     }
-    public int calculateMainNum(){
+    private int calculateMainNum(){
         int mainNum=0;
         for(MenuDetail menuDetail:menuDetailList){
             if(MenuGroup.findByMenuList(menuDetail)== MenuGroup.MAIN){
